4d469cf49f3122ae3ecc8d320d327e4ba1fe7fde
pillowek <vaxiie@centrum.cz>
2023-11-16 10:24:38 +0100

    Xasd

--- src/main/java/me/school/bank/BankInjector.java
+++ src/main/java/me/school/bank/BankInjector.java
@@ -1,11 +1,33 @@
 package me.school.bank;
 
+import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.InjectionListener;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 import me.school.bank.print.ConsolePrintService;
 import me.school.bank.print.PrintInterface;
 
 public class BankInjector extends AbstractModule {
+    private EventBus eventBus = new EventBus("Default EventBus");
+
     @Override protected void configure() {
         this.bind(PrintInterface.class).to(ConsolePrintService.class);
+        this.bind(EventBus.class).toInstance(eventBus);
+
+        bindListener(Matchers.any(), new TypeListener() {
+
+            @Override
+            public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
+                encounter.register(new InjectionListener<I>() {
+                    @Override
+                    public void afterInjection(I injectee) {
+                        eventBus.register(injectee);
+                    }
+                });
+            }
+        });
     }
 }
--- src/main/java/me/school/bank/Main.java
+++ src/main/java/me/school/bank/Main.java
@@ -2,12 +2,16 @@ package me.school.bank;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import me.school.bank.notification.NotificationCustomerEvent;
+import me.school.bank.notification.NotifyCustomerEventListener;
 
 public class Main {
     public static void main(String args[]) throws Exception {
         try {
             Injector injector = Guice.createInjector(new BankInjector());
             Bank bank = injector.getInstance(Bank.class);
+
+            injector.getInstance(NotifyCustomerEventListener.class);
 
             bank.run();
         } catch (Exception e) {
